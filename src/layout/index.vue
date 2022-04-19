a943445b8df79e244066401ab1a9184711150927
zhengwei <zhengwei@dusto-yc.com>
2022-04-20 00:01:22 +0800

    sidebar css 1

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="top-header" />
+<!--    <div class="top-header" />-->
     <div :class="classObj" class="app-wrapper">
       <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
       <sidebar class="sidebar-container" />
